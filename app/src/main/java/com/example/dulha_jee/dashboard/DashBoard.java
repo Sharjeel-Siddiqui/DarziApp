96764a9125ce3de524f084fd4d32cf5433690f47
Sharjeel-Siddiqui <sharjeelahmadsiddiqui@gmail.com>
2021-01-05 00:12:24 +0500

    Second Commit - 4 - january -2021
    Kurta Fragment ui completed along with all field described by client

--- app/src/main/java/com/example/dulha_jee/dashboard/DashBoard.java
+++ app/src/main/java/com/example/dulha_jee/dashboard/DashBoard.java
@@ -7,20 +7,65 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.dulha_jee.R;
 
-public class DashBoard extends Fragment{
+public class DashBoard extends Fragment implements View.OnClickListener {
+
+    CardView card_kurta, card_shirwani, card_coat, card_waistCoat, card_pant, card_shirt;
+    NavController navController;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dashboard,container,false);
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initViews(view);
+    }
+
+    private void initViews(View view) {
+        navController = Navigation.findNavController(view);
+        card_kurta = view.findViewById(R.id.card_kurta);
+        card_shirwani = view.findViewById(R.id.card_sherwani);
+        card_waistCoat = view.findViewById(R.id.card_waistcoat);
+        card_coat = view.findViewById(R.id.card_coat);
+        card_pant = view.findViewById(R.id.card_pant);
+        card_shirt = view.findViewById(R.id.card_shirt);
+
+        card_kurta.setOnClickListener(this);
+        card_shirwani.setOnClickListener(this);
+        card_waistCoat.setOnClickListener(this);
+        card_coat.setOnClickListener(this);
+        card_pant.setOnClickListener(this);
+        card_shirt.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.card_kurta:
+                navController.navigate(R.id.action_dashBoard_to_kurtaFragment);
+                break;
+            case R.id.card_sherwani:
+                break;
+            case R.id.card_waistcoat:
+                break;
+            case R.id.card_coat:
+                break;
+            case R.id.card_pant:
+                break;
+            case R.id.card_shirt:
+                break;
+            default:
+                //do nothing
+        }
     }
 }
