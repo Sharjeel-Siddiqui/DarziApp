b331db43e437c4f4b2d4dd3683e4eff5a1494cb6
Sharjeel-Siddiqui <sharjeelahmadsiddiqui@gmail.com>
2021-01-10 22:10:11 +0500

    Seventh Commit  10 - january -2021
    Coat Ui complete

--- app/src/main/java/com/example/dulha_jee/dashboard/FragmentCoat.java
+++ app/src/main/java/com/example/dulha_jee/dashboard/FragmentCoat.java
@@ -4,30 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.dulha_jee.MainActivity;
 import com.example.dulha_jee.R;
 
-public class KurtaFragment extends Fragment {
-
-    Spinner dropdown_kurta_varieties;
+public class FragmentCoat extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_kurta,container,false);
+        return inflater.inflate(R.layout.fragment_coat,container,false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        ((MainActivity) getActivity()).setToolbar("Kurta...");
-        dropdown_kurta_varieties = view.findViewById(R.id.dropdown_kurta_varieties);
-
     }
 }
