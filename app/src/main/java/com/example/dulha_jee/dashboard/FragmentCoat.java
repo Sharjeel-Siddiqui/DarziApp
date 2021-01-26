ea4dfdab39501a69f7a7cd22b4a45e73511123fb
Sharjeel-Siddiqui <sharjeelahmadsiddiqui@gmail.com>
2021-01-26 22:52:22 +0500

    26 Commit  24 - january -2021
    Coat field id and request body

--- app/src/main/java/com/example/dulha_jee/dashboard/FragmentCoat.java
+++ app/src/main/java/com/example/dulha_jee/dashboard/FragmentCoat.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
@@ -20,12 +22,163 @@ import com.example.dulha_jee.R;
 import com.tapadoo.alerter.Alerter;
 import com.tapadoo.alerter.OnHideAlertListener;
 
+import butterknife.BindView;
+
 public class FragmentCoat extends Fragment {
     Spinner dropdown_karegar_name, dropdown_coat_varieties;
     String[] karegarName = {" کاریگر کا نام", "ابرار ", "احمد ", "امین ", "عارف "};
-    String[] coatVarieties = { "گون اسٹائل فرنٹ اوپن کوٹ ", "پرنس کوٹ ", "کوٹ "};
+    String[] coatVarieties = {"گون اسٹائل فرنٹ اوپن کوٹ ", "پرنس کوٹ ", "کوٹ "};
     Button submit_coat;
     NavController navController;
+
+    //fields to bind view
+    @BindView(R.id.quantity)
+    EditText quantity;
+    @BindView(R.id.collar)
+    EditText collar;
+    @BindView(R.id.sleeves)
+    EditText sleeves;
+    @BindView(R.id.shoulder)
+    EditText shoulder;
+    @BindView(R.id.hip)
+    EditText hip;
+    @BindView(R.id.abdomen)
+    EditText abdomen;
+    @BindView(R.id.gudda)
+    EditText gudda;
+    @BindView(R.id.chest)
+    EditText chest;
+    @BindView(R.id.lengthMade)
+    EditText lengthMade;
+    @BindView(R.id.full_back)
+    EditText full_back;
+    @BindView(R.id.halfback)
+    EditText halfback;
+    @BindView(R.id.crossfront)
+    EditText crossfront;
+    @BindView(R.id.chowk_length)
+    EditText chowk_length;
+    @BindView(R.id.pencil_length)
+    EditText pencil_length;
+    @BindView(R.id.collar_thing)
+    EditText collar_thing;
+    @BindView(R.id.pencil_thing)
+    EditText pencil_thing;
+    @BindView(R.id.shoulder_depth)
+    EditText shoulder_depth;
+    @BindView(R.id.front_cadge)
+    EditText front_cadge;
+    @BindView(R.id.order_date)
+    EditText order_date;
+    @BindView(R.id.order_date_most_urgent)
+    EditText order_date_most_urgent;
+    @BindView(R.id.remarks)
+    EditText remarks;
+
+
+    //checkboxes
+    @BindView(R.id.button_style2)
+    CheckBox button_style2;
+    @BindView(R.id.single_button)
+    CheckBox single_button;
+    @BindView(R.id.three_button)
+    CheckBox three_button;
+    @BindView(R.id.four_button)
+    CheckBox four_button;
+    @BindView(R.id.five_button)
+    CheckBox five_button;
+    @BindView(R.id.no_button)
+    CheckBox no_button;
+    @BindView(R.id.side_chowk)
+    CheckBox side_chowk;
+    @BindView(R.id.back_chowk)
+    CheckBox back_chowk;
+    @BindView(R.id.american_pencil)
+    CheckBox american_pencil;
+    @BindView(R.id.dinner_pencil)
+    CheckBox dinner_pencil;
+    @BindView(R.id.pencil_long)
+    CheckBox pencil_long;
+    @BindView(R.id.taxido_style)
+    CheckBox taxido_style;
+    @BindView(R.id.double_style_collar)
+    CheckBox double_style_collar;
+    @BindView(R.id.qub)
+    CheckBox qub;
+    @BindView(R.id.no_meat)
+    CheckBox no_meat;
+    @BindView(R.id.waist_coat_double)
+    CheckBox waist_coat_double;
+    @BindView(R.id.pencil_waist_coat)
+    CheckBox pencil_waist_coat;
+    @BindView(R.id.v_neck_waist_coat)
+    CheckBox v_neck_waist_coat;
+    @BindView(R.id.u_neck_waist_coat)
+    CheckBox u_neck_waist_coat;
+    @BindView(R.id.same_as_image)
+    CheckBox same_as_image;
+    @BindView(R.id.coat_same_as_image)
+    CheckBox coat_same_as_image;
+    @BindView(R.id.waist_coat_same_as_image)
+    CheckBox waist_coat_same_as_image;
+    @BindView(R.id.inner_loozing)
+    CheckBox inner_loozing;
+    @BindView(R.id.special_vip)
+    CheckBox special_vip;
+    @BindView(R.id.sleeves_loozing)
+    CheckBox sleeves_loozing;
+    @BindView(R.id.double_bukram)
+    CheckBox double_bukram;
+    @BindView(R.id.full_bukram)
+    CheckBox full_bukram;
+    @BindView(R.id.astar_printed)
+    CheckBox astar_printed;
+    @BindView(R.id.prince_coat)
+    CheckBox prince_coat;
+    @BindView(R.id.fancy_buttons)
+    CheckBox fancy_buttons;
+    @BindView(R.id.customer_cloth)
+    CheckBox customer_cloth;
+    @BindView(R.id.only_sewing)
+    CheckBox only_sewing;
+    @BindView(R.id.child_kurta_size)
+    CheckBox child_kurta_size;
+    @BindView(R.id.finished_adjust)
+    CheckBox finished_adjust;
+    @BindView(R.id.special_customer_order)
+    CheckBox special_customer_order;
+    @BindView(R.id.regular_customer_order)
+    CheckBox regular_customer_order;
+    @BindView(R.id.urgent_order)
+    CheckBox urgent_order;
+    @BindView(R.id.no_label)
+    CheckBox no_label;
+    @BindView(R.id.special_order)
+    CheckBox special_order;
+    @BindView(R.id.button_should_be_strong)
+    CheckBox button_should_be_strong;
+    @BindView(R.id.shoulder_down)
+    CheckBox shoulder_down;
+    @BindView(R.id.light_work_shoulder_down)
+    CheckBox light_work_shoulder_down;
+    @BindView(R.id.full_shoulder_down)
+    CheckBox full_shoulder_down;
+    @BindView(R.id.straight_shoulder)
+    CheckBox straight_shoulder;
+    @BindView(R.id.right_shoulder_down)
+    CheckBox right_shoulder_down;
+    @BindView(R.id.left_shoulder_down)
+    CheckBox left_shoulder_down;
+    @BindView(R.id.altered_body)
+    CheckBox altered_body;
+    @BindView(R.id.deep_body)
+    CheckBox deep_body;
+    @BindView(R.id.party_label)
+    CheckBox party_label;
+    @BindView(R.id.fancy_label)
+    CheckBox fancy_label;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -64,7 +217,7 @@ public class FragmentCoat extends Fragment {
                         .setOnHideListener(new OnHideAlertListener() {
                             @Override
                             public void onHide() {
-                                navController.navigate(R.id.action_fragmentCoat_to_dashBoard,null, new NavOptions.Builder()
+                                navController.navigate(R.id.action_fragmentCoat_to_dashBoard, null, new NavOptions.Builder()
                                         .setPopUpTo(R.id.fragmentCoat,
                                                 true).build());
                             }
---
+++ app/src/main/java/com/example/dulha_jee/pojo/CoatRequestBody.java
@@ -0,0 +1,657 @@
+package com.example.dulha_jee.pojo;
+
+public class CoatRequestBody {
+    private String quantity , collar,  sleeves, shoulder ,hip ,abdomen, gudda, chest, lengthMade, full_back, halfback, crossfront ,chowk_length, pencil_length, collar_thing ,pencil_thing ,shoulder_depth ,front_cadge ,order_date, order_date_most_urgent, remarks , button_style2 , single_button, three_button ,four_button, five_button, no_button ,side_chowk, back_chowk ,american_pencil, dinner_pencil, pencil_long ,taxido_style, double_style_collar, qub ,no_meat, waist_coat_double, pencil_waist_coat ,v_neck_waist_coat, u_neck_waist_coat ,same_as_image, coat_same_as_image, waist_coat_same_as_image ,inner_loozing ,special_vip, sleeves_,loozing, double_bukram, full_bukram, astar_printed, prince_coat ,fancy_buttons ,customer_cloth, only_sewing, child_kurta_size, finished_adjust, special_customer_order, regular_customer_order ,urgent_order, no_label ,special_order ,button_should_be_strong, shoulder_down, light_work_shoulder_down, full_shoulder_down, straight_shoulder, right_shoulder_down ,left_shoulder_down ,altered_body ,deep_body, party_label, fancy_label;
+
+
+    public CoatRequestBody(String quantity, String collar, String sleeves, String shoulder, String hip, String abdomen, String gudda, String chest, String lengthMade, String full_back, String halfback, String crossfront, String chowk_length, String pencil_length, String collar_thing, String pencil_thing, String shoulder_depth, String front_cadge, String order_date, String order_date_most_urgent, String remarks, String button_style2, String single_button, String three_button, String four_button, String five_button, String no_button, String side_chowk, String back_chowk, String american_pencil, String dinner_pencil, String pencil_long, String taxido_style, String double_style_collar, String qub, String no_meat, String waist_coat_double, String pencil_waist_coat, String v_neck_waist_coat, String u_neck_waist_coat, String same_as_image, String coat_same_as_image, String waist_coat_same_as_image, String inner_loozing, String special_vip, String sleeves_, String loozing, String double_bukram, String full_bukram, String astar_printed, String prince_coat, String fancy_buttons, String customer_cloth, String only_sewing, String child_kurta_size, String finished_adjust, String special_customer_order, String regular_customer_order, String urgent_order, String no_label, String special_order, String button_should_be_strong, String shoulder_down, String light_work_shoulder_down, String full_shoulder_down, String straight_shoulder, String right_shoulder_down, String left_shoulder_down, String altered_body, String deep_body, String party_label, String fancy_label) {
+        this.quantity = quantity;
+        this.collar = collar;
+        this.sleeves = sleeves;
+        this.shoulder = shoulder;
+        this.hip = hip;
+        this.abdomen = abdomen;
+        this.gudda = gudda;
+        this.chest = chest;
+        this.lengthMade = lengthMade;
+        this.full_back = full_back;
+        this.halfback = halfback;
+        this.crossfront = crossfront;
+        this.chowk_length = chowk_length;
+        this.pencil_length = pencil_length;
+        this.collar_thing = collar_thing;
+        this.pencil_thing = pencil_thing;
+        this.shoulder_depth = shoulder_depth;
+        this.front_cadge = front_cadge;
+        this.order_date = order_date;
+        this.order_date_most_urgent = order_date_most_urgent;
+        this.remarks = remarks;
+        this.button_style2 = button_style2;
+        this.single_button = single_button;
+        this.three_button = three_button;
+        this.four_button = four_button;
+        this.five_button = five_button;
+        this.no_button = no_button;
+        this.side_chowk = side_chowk;
+        this.back_chowk = back_chowk;
+        this.american_pencil = american_pencil;
+        this.dinner_pencil = dinner_pencil;
+        this.pencil_long = pencil_long;
+        this.taxido_style = taxido_style;
+        this.double_style_collar = double_style_collar;
+        this.qub = qub;
+        this.no_meat = no_meat;
+        this.waist_coat_double = waist_coat_double;
+        this.pencil_waist_coat = pencil_waist_coat;
+        this.v_neck_waist_coat = v_neck_waist_coat;
+        this.u_neck_waist_coat = u_neck_waist_coat;
+        this.same_as_image = same_as_image;
+        this.coat_same_as_image = coat_same_as_image;
+        this.waist_coat_same_as_image = waist_coat_same_as_image;
+        this.inner_loozing = inner_loozing;
+        this.special_vip = special_vip;
+        this.sleeves_ = sleeves_;
+        this.loozing = loozing;
+        this.double_bukram = double_bukram;
+        this.full_bukram = full_bukram;
+        this.astar_printed = astar_printed;
+        this.prince_coat = prince_coat;
+        this.fancy_buttons = fancy_buttons;
+        this.customer_cloth = customer_cloth;
+        this.only_sewing = only_sewing;
+        this.child_kurta_size = child_kurta_size;
+        this.finished_adjust = finished_adjust;
+        this.special_customer_order = special_customer_order;
+        this.regular_customer_order = regular_customer_order;
+        this.urgent_order = urgent_order;
+        this.no_label = no_label;
+        this.special_order = special_order;
+        this.button_should_be_strong = button_should_be_strong;
+        this.shoulder_down = shoulder_down;
+        this.light_work_shoulder_down = light_work_shoulder_down;
+        this.full_shoulder_down = full_shoulder_down;
+        this.straight_shoulder = straight_shoulder;
+        this.right_shoulder_down = right_shoulder_down;
+        this.left_shoulder_down = left_shoulder_down;
+        this.altered_body = altered_body;
+        this.deep_body = deep_body;
+        this.party_label = party_label;
+        this.fancy_label = fancy_label;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCollar() {
+        return collar;
+    }
+
+    public void setCollar(String collar) {
+        this.collar = collar;
+    }
+
+    public String getSleeves() {
+        return sleeves;
+    }
+
+    public void setSleeves(String sleeves) {
+        this.sleeves = sleeves;
+    }
+
+    public String getShoulder() {
+        return shoulder;
+    }
+
+    public void setShoulder(String shoulder) {
+        this.shoulder = shoulder;
+    }
+
+    public String getHip() {
+        return hip;
+    }
+
+    public void setHip(String hip) {
+        this.hip = hip;
+    }
+
+    public String getAbdomen() {
+        return abdomen;
+    }
+
+    public void setAbdomen(String abdomen) {
+        this.abdomen = abdomen;
+    }
+
+    public String getGudda() {
+        return gudda;
+    }
+
+    public void setGudda(String gudda) {
+        this.gudda = gudda;
+    }
+
+    public String getChest() {
+        return chest;
+    }
+
+    public void setChest(String chest) {
+        this.chest = chest;
+    }
+
+    public String getLengthMade() {
+        return lengthMade;
+    }
+
+    public void setLengthMade(String lengthMade) {
+        this.lengthMade = lengthMade;
+    }
+
+    public String getFull_back() {
+        return full_back;
+    }
+
+    public void setFull_back(String full_back) {
+        this.full_back = full_back;
+    }
+
+    public String getHalfback() {
+        return halfback;
+    }
+
+    public void setHalfback(String halfback) {
+        this.halfback = halfback;
+    }
+
+    public String getCrossfront() {
+        return crossfront;
+    }
+
+    public void setCrossfront(String crossfront) {
+        this.crossfront = crossfront;
+    }
+
+    public String getChowk_length() {
+        return chowk_length;
+    }
+
+    public void setChowk_length(String chowk_length) {
+        this.chowk_length = chowk_length;
+    }
+
+    public String getPencil_length() {
+        return pencil_length;
+    }
+
+    public void setPencil_length(String pencil_length) {
+        this.pencil_length = pencil_length;
+    }
+
+    public String getCollar_thing() {
+        return collar_thing;
+    }
+
+    public void setCollar_thing(String collar_thing) {
+        this.collar_thing = collar_thing;
+    }
+
+    public String getPencil_thing() {
+        return pencil_thing;
+    }
+
+    public void setPencil_thing(String pencil_thing) {
+        this.pencil_thing = pencil_thing;
+    }
+
+    public String getShoulder_depth() {
+        return shoulder_depth;
+    }
+
+    public void setShoulder_depth(String shoulder_depth) {
+        this.shoulder_depth = shoulder_depth;
+    }
+
+    public String getFront_cadge() {
+        return front_cadge;
+    }
+
+    public void setFront_cadge(String front_cadge) {
+        this.front_cadge = front_cadge;
+    }
+
+    public String getOrder_date() {
+        return order_date;
+    }
+
+    public void setOrder_date(String order_date) {
+        this.order_date = order_date;
+    }
+
+    public String getOrder_date_most_urgent() {
+        return order_date_most_urgent;
+    }
+
+    public void setOrder_date_most_urgent(String order_date_most_urgent) {
+        this.order_date_most_urgent = order_date_most_urgent;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getButton_style2() {
+        return button_style2;
+    }
+
+    public void setButton_style2(String button_style2) {
+        this.button_style2 = button_style2;
+    }
+
+    public String getSingle_button() {
+        return single_button;
+    }
+
+    public void setSingle_button(String single_button) {
+        this.single_button = single_button;
+    }
+
+    public String getThree_button() {
+        return three_button;
+    }
+
+    public void setThree_button(String three_button) {
+        this.three_button = three_button;
+    }
+
+    public String getFour_button() {
+        return four_button;
+    }
+
+    public void setFour_button(String four_button) {
+        this.four_button = four_button;
+    }
+
+    public String getFive_button() {
+        return five_button;
+    }
+
+    public void setFive_button(String five_button) {
+        this.five_button = five_button;
+    }
+
+    public String getNo_button() {
+        return no_button;
+    }
+
+    public void setNo_button(String no_button) {
+        this.no_button = no_button;
+    }
+
+    public String getSide_chowk() {
+        return side_chowk;
+    }
+
+    public void setSide_chowk(String side_chowk) {
+        this.side_chowk = side_chowk;
+    }
+
+    public String getBack_chowk() {
+        return back_chowk;
+    }
+
+    public void setBack_chowk(String back_chowk) {
+        this.back_chowk = back_chowk;
+    }
+
+    public String getAmerican_pencil() {
+        return american_pencil;
+    }
+
+    public void setAmerican_pencil(String american_pencil) {
+        this.american_pencil = american_pencil;
+    }
+
+    public String getDinner_pencil() {
+        return dinner_pencil;
+    }
+
+    public void setDinner_pencil(String dinner_pencil) {
+        this.dinner_pencil = dinner_pencil;
+    }
+
+    public String getPencil_long() {
+        return pencil_long;
+    }
+
+    public void setPencil_long(String pencil_long) {
+        this.pencil_long = pencil_long;
+    }
+
+    public String getTaxido_style() {
+        return taxido_style;
+    }
+
+    public void setTaxido_style(String taxido_style) {
+        this.taxido_style = taxido_style;
+    }
+
+    public String getDouble_style_collar() {
+        return double_style_collar;
+    }
+
+    public void setDouble_style_collar(String double_style_collar) {
+        this.double_style_collar = double_style_collar;
+    }
+
+    public String getQub() {
+        return qub;
+    }
+
+    public void setQub(String qub) {
+        this.qub = qub;
+    }
+
+    public String getNo_meat() {
+        return no_meat;
+    }
+
+    public void setNo_meat(String no_meat) {
+        this.no_meat = no_meat;
+    }
+
+    public String getWaist_coat_double() {
+        return waist_coat_double;
+    }
+
+    public void setWaist_coat_double(String waist_coat_double) {
+        this.waist_coat_double = waist_coat_double;
+    }
+
+    public String getPencil_waist_coat() {
+        return pencil_waist_coat;
+    }
+
+    public void setPencil_waist_coat(String pencil_waist_coat) {
+        this.pencil_waist_coat = pencil_waist_coat;
+    }
+
+    public String getV_neck_waist_coat() {
+        return v_neck_waist_coat;
+    }
+
+    public void setV_neck_waist_coat(String v_neck_waist_coat) {
+        this.v_neck_waist_coat = v_neck_waist_coat;
+    }
+
+    public String getU_neck_waist_coat() {
+        return u_neck_waist_coat;
+    }
+
+    public void setU_neck_waist_coat(String u_neck_waist_coat) {
+        this.u_neck_waist_coat = u_neck_waist_coat;
+    }
+
+    public String getSame_as_image() {
+        return same_as_image;
+    }
+
+    public void setSame_as_image(String same_as_image) {
+        this.same_as_image = same_as_image;
+    }
+
+    public String getCoat_same_as_image() {
+        return coat_same_as_image;
+    }
+
+    public void setCoat_same_as_image(String coat_same_as_image) {
+        this.coat_same_as_image = coat_same_as_image;
+    }
+
+    public String getWaist_coat_same_as_image() {
+        return waist_coat_same_as_image;
+    }
+
+    public void setWaist_coat_same_as_image(String waist_coat_same_as_image) {
+        this.waist_coat_same_as_image = waist_coat_same_as_image;
+    }
+
+    public String getInner_loozing() {
+        return inner_loozing;
+    }
+
+    public void setInner_loozing(String inner_loozing) {
+        this.inner_loozing = inner_loozing;
+    }
+
+    public String getSpecial_vip() {
+        return special_vip;
+    }
+
+    public void setSpecial_vip(String special_vip) {
+        this.special_vip = special_vip;
+    }
+
+    public String getSleeves_() {
+        return sleeves_;
+    }
+
+    public void setSleeves_(String sleeves_) {
+        this.sleeves_ = sleeves_;
+    }
+
+    public String getLoozing() {
+        return loozing;
+    }
+
+    public void setLoozing(String loozing) {
+        this.loozing = loozing;
+    }
+
+    public String getDouble_bukram() {
+        return double_bukram;
+    }
+
+    public void setDouble_bukram(String double_bukram) {
+        this.double_bukram = double_bukram;
+    }
+
+    public String getFull_bukram() {
+        return full_bukram;
+    }
+
+    public void setFull_bukram(String full_bukram) {
+        this.full_bukram = full_bukram;
+    }
+
+    public String getAstar_printed() {
+        return astar_printed;
+    }
+
+    public void setAstar_printed(String astar_printed) {
+        this.astar_printed = astar_printed;
+    }
+
+    public String getPrince_coat() {
+        return prince_coat;
+    }
+
+    public void setPrince_coat(String prince_coat) {
+        this.prince_coat = prince_coat;
+    }
+
+    public String getFancy_buttons() {
+        return fancy_buttons;
+    }
+
+    public void setFancy_buttons(String fancy_buttons) {
+        this.fancy_buttons = fancy_buttons;
+    }
+
+    public String getCustomer_cloth() {
+        return customer_cloth;
+    }
+
+    public void setCustomer_cloth(String customer_cloth) {
+        this.customer_cloth = customer_cloth;
+    }
+
+    public String getOnly_sewing() {
+        return only_sewing;
+    }
+
+    public void setOnly_sewing(String only_sewing) {
+        this.only_sewing = only_sewing;
+    }
+
+    public String getChild_kurta_size() {
+        return child_kurta_size;
+    }
+
+    public void setChild_kurta_size(String child_kurta_size) {
+        this.child_kurta_size = child_kurta_size;
+    }
+
+    public String getFinished_adjust() {
+        return finished_adjust;
+    }
+
+    public void setFinished_adjust(String finished_adjust) {
+        this.finished_adjust = finished_adjust;
+    }
+
+    public String getSpecial_customer_order() {
+        return special_customer_order;
+    }
+
+    public void setSpecial_customer_order(String special_customer_order) {
+        this.special_customer_order = special_customer_order;
+    }
+
+    public String getRegular_customer_order() {
+        return regular_customer_order;
+    }
+
+    public void setRegular_customer_order(String regular_customer_order) {
+        this.regular_customer_order = regular_customer_order;
+    }
+
+    public String getUrgent_order() {
+        return urgent_order;
+    }
+
+    public void setUrgent_order(String urgent_order) {
+        this.urgent_order = urgent_order;
+    }
+
+    public String getNo_label() {
+        return no_label;
+    }
+
+    public void setNo_label(String no_label) {
+        this.no_label = no_label;
+    }
+
+    public String getSpecial_order() {
+        return special_order;
+    }
+
+    public void setSpecial_order(String special_order) {
+        this.special_order = special_order;
+    }
+
+    public String getButton_should_be_strong() {
+        return button_should_be_strong;
+    }
+
+    public void setButton_should_be_strong(String button_should_be_strong) {
+        this.button_should_be_strong = button_should_be_strong;
+    }
+
+    public String getShoulder_down() {
+        return shoulder_down;
+    }
+
+    public void setShoulder_down(String shoulder_down) {
+        this.shoulder_down = shoulder_down;
+    }
+
+    public String getLight_work_shoulder_down() {
+        return light_work_shoulder_down;
+    }
+
+    public void setLight_work_shoulder_down(String light_work_shoulder_down) {
+        this.light_work_shoulder_down = light_work_shoulder_down;
+    }
+
+    public String getFull_shoulder_down() {
+        return full_shoulder_down;
+    }
+
+    public void setFull_shoulder_down(String full_shoulder_down) {
+        this.full_shoulder_down = full_shoulder_down;
+    }
+
+    public String getStraight_shoulder() {
+        return straight_shoulder;
+    }
+
+    public void setStraight_shoulder(String straight_shoulder) {
+        this.straight_shoulder = straight_shoulder;
+    }
+
+    public String getRight_shoulder_down() {
+        return right_shoulder_down;
+    }
+
+    public void setRight_shoulder_down(String right_shoulder_down) {
+        this.right_shoulder_down = right_shoulder_down;
+    }
+
+    public String getLeft_shoulder_down() {
+        return left_shoulder_down;
+    }
+
+    public void setLeft_shoulder_down(String left_shoulder_down) {
+        this.left_shoulder_down = left_shoulder_down;
+    }
+
+    public String getAltered_body() {
+        return altered_body;
+    }
+
+    public void setAltered_body(String altered_body) {
+        this.altered_body = altered_body;
+    }
+
+    public String getDeep_body() {
+        return deep_body;
+    }
+
+    public void setDeep_body(String deep_body) {
+        this.deep_body = deep_body;
+    }
+
+    public String getParty_label() {
+        return party_label;
+    }
+
+    public void setParty_label(String party_label) {
+        this.party_label = party_label;
+    }
+
+    public String getFancy_label() {
+        return fancy_label;
+    }
+
+    public void setFancy_label(String fancy_label) {
+        this.fancy_label = fancy_label;
+    }
+}
